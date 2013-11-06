245609a5f835bb899b66a2dafdb36749b2c2ddd9
ufo22940268 <ufo22940268@gmail.com>
2013-11-06 08:02:21 +0800

    asdf

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from rms import deploy
 from eve_docs import eve_docs
 from flask.ext.bootstrap import Bootstrap
 
-host = deploy.get_host()
+#host = deploy.get_host()
 port = 5000
 Bootstrap(rms.app)
 rms.app.register_blueprint(eve_docs, url_prefix='/docs')
-rms.app.run(host=host, port=port, debug=True)
+rms.app.run(port=port, debug=True)
